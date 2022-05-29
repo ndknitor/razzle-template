89d8bdb3a3bce6bbec75306ca3f75e710ffed083
KN <nguyentuanpkn@gmail.com>
2022-05-29 22:36:40 +0700

    Fix authorized view and non authorized view

--- src/shared/NonAuthorizeView.tsx
+++ src/shared/NonAuthorizeView.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, PropsWithChildren } from 'react'
-import useAuth from '../hook/useAuth'
+import useAuth from '../contexthook/useAuth';
 
 function NonAuthorizeView(props:PropsWithChildren<{}>) {
     const {authenticated} = useAuth();
