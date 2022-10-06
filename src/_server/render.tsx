d66e2cf0a469bf0c3826623b3562a8d0fcad73b1
ndkn <nguyentuanpkn@gmail.com>
2022-10-06 23:50:38 +0700

    Fix some error

--- src/_server/render.tsx
+++ src/_server/render.tsx
@@ -1,5 +1,9 @@
 import express from 'express';
+import { StaticRouter } from 'react-router-dom/server';
+import { renderToString } from 'react-dom/server';
 import { Helmet } from 'react-helmet';
+import React from 'react';
+import App from '../App';
 let assets: any;
 const syncLoadAssets = () => {
   assets = require(process.env.RAZZLE_ASSETS_MANIFEST!);
@@ -7,30 +11,29 @@ const syncLoadAssets = () => {
 syncLoadAssets();
 
 const cssLinksFromAssets = (assets, entrypoint) => {
-    return assets[entrypoint] ? assets[entrypoint].css ?
-      assets[entrypoint].css.map(asset =>
-        `<link rel="stylesheet" href="${asset}">`
-      ).join('') : '' : '';
-  };
-  
-  const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
-    return assets[entrypoint] ? assets[entrypoint].js ?
-      assets[entrypoint].js.map(asset =>
-        `<script src="${asset}"${extra}></script>`
-      ).join('') : '' : '';
-  };
+  return assets[entrypoint] ? assets[entrypoint].css ?
+    assets[entrypoint].css.map(asset =>
+      `<link rel="stylesheet" href="${asset}">`
+    ).join('') : '' : '';
+};
+
+const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
+  return assets[entrypoint] ? assets[entrypoint].js ?
+    assets[entrypoint].js.map(asset =>
+      `<script src="${asset}"${extra}></script>`
+    ).join('') : '' : '';
+};
 
 export const renderApp = (req: express.Request) => {
-    // const markup = renderToString(
-    //     <StaticRouter location={req.url}>
-    //       <App />
-    //     </StaticRouter>
-    // );
-    const markup = '';
-    const helmet = Helmet.renderStatic();
-    const html =
-      // prettier-ignore
-      `<!doctype html>
+  const markup = renderToString(
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
+  const helmet = Helmet.renderStatic();
+  const html =
+    // prettier-ignore
+    `<!doctype html>
       <html lang="">
       <head>
           ${helmet.title.toString()}
@@ -46,5 +49,5 @@ export const renderApp = (req: express.Request) => {
           ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
       </body>
     </html>`;
-    return html;
-  };
+  return html;
+};
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from '../page/About';
-import Index from '../page/Index';
+import About from '../pages/About';
+import Index from '../pages/Index';
+
 function AppRouter() {
   return (
-      <Routes>
-        <Route path='/' element={<Index />}></Route>
-        <Route path='/about' element={<About/>}/>
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Index />}></Route>
+      <Route path='/about' element={<About />} />
+    </Routes>
   )
 }
 
