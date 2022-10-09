dc8f9038ba96f4aa830029d3a3671c2e3e68a779
ndkn <nguyentuanpkn@gmail.com>
2022-10-10 00:05:14 +0700

    Change render type

--- src/client.tsx
+++ src/client.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createBroswerContext } from 'use-sse';
 
 import App from './App';
 const BroswerDataContext = createBroswerContext();
-
-hydrate(
+createRoot(document.getElementById('root') as HTMLElement)
+.render(
   <BrowserRouter>
     <BroswerDataContext>
       <App />
     </BroswerDataContext >
   </BrowserRouter>
-  ,
-  document.getElementById('root')
 );
 
 if (module.hot) {
